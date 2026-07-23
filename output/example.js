bdb3eaf8925678d8e29d0e2f7d3c44c294fea77c
faridmia <mdfarid7830@gmail.com>
2026-07-23 21:50:26 +0600

    type objeect

--- output/example.js
+++ output/example.js
@@ -1,5 +1,23 @@
 "use strict";
 let fullName = "John Doe";
-console.log(fullName);
+console.log(fullName.toUpperCase);
 let age = 30;
 console.log(age);
+let projectCompleted = true;
+console.log(projectCompleted);
+const names = [];
+names.push("Alice");
+names.push("Bob");
+console.log(names);
+const user = {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com"
+};
+const person = {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com"
+};
+console.log(user);
+console.log(person);
--- src/example.ts
+++ src/example.ts
@@ -5,3 +5,37 @@ console.log(fullName.toUpperCase);
 let age: number = 30;
 
 console.log(age);
+
+let projectCompleted: boolean = true;
+console.log(projectCompleted);
+
+
+
+const names: string[] = [];
+
+names.push("Alice");
+names.push("Bob"); 
+
+console.log(names);
+
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+};
+
+const user: User = {    
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com"
+};
+
+const person: User = {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com"
+};
+
+console.log(user);
+console.log(person);
